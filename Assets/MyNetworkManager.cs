3d9e17342e0d77bd4ea1b5f04fdb4a8aafbde3aa
DANIEL-FG\o9708 <o97082@yahoo.com.tw>
2018-03-29 00:12:15 +0800

    Added network manager

---
+++ Assets/MyNetworkManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class MyNetworkManager : NetworkManager
+{
+    private void Start()
+    {
+
+    }
+    
+    public void MyStartHost()
+    {
+        Debug.Log("Time StartHost: " + Time.timeSinceLevelLoad);
+        StartHost();
+    }
+
+    public override void OnStartHost()
+    {
+        Debug.Log("Time OnStartHost: " + Time.timeSinceLevelLoad);
+    }
+}
