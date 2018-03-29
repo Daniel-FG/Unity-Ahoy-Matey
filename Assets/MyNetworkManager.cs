991f3d7ffa7399b8913cbcc21426eef5023bdd9c
DANIEL-FG\o9708 <o97082@yahoo.com.tw>
2018-03-29 16:50:38 +0800

    Added multiplayer networking

--- Assets/MyNetworkManager.cs
+++ Assets/MyNetworkManager.cs
@@ -5,19 +5,33 @@ using UnityEngine.Networking;
 
 public class MyNetworkManager : NetworkManager
 {
-    private void Start()
-    {
-
-    }
-    
     public void MyStartHost()
     {
         Debug.Log("Time StartHost: " + Time.timeSinceLevelLoad);
         StartHost();
     }
-
+    public void MyStartClient()
+    {
+        Debug.Log("Time StartHost: " + Time.timeSinceLevelLoad);
+        StartClient();
+    }
     public override void OnStartHost()
     {
         Debug.Log("Time OnStartHost: " + Time.timeSinceLevelLoad);
+    }
+    public override void OnStartClient(NetworkClient client)
+    {
+        Debug.Log("Start Client " + Time.timeSinceLevelLoad);
+        InvokeRepeating("PrintDots", 0f, 1f);
+    }
+    public override void OnClientConnect(NetworkConnection conn)
+    {
+        Debug.Log("Client is connected to " + conn.address);
+        CancelInvoke();
+    }
+
+    void PrintDots()
+    {
+        Debug.Log(".");
     }
 }
---
+++ Assets/Player.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : NetworkBehaviour
+{
+	void Update ()
+    {
+        if (!isLocalPlayer)
+        {
+            return;
+        }
+        float xpos = CrossPlatformInputManager.GetAxis("Horizontal");
+        float zpos = CrossPlatformInputManager.GetAxis("Vertical");
+        transform.position = new Vector3(transform.position.x + xpos, transform.position.y, transform.position.z + zpos);
+	}
+}
